731c19f9e8f5220827b43bc259b26c1cc25c6625
Destineegibson5 <43441789+Destineegibson5@users.noreply.github.com>
2019-06-23 14:13:25 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,86 @@
+// DECLARE YOUR VARIABLES HERE
+
+var teamOneName ="Coffee";
+var teamTwoName= "Tea";
+var scoreOne=0;
+var scoreTwo=0;
+var freeThrow=2;
+var twoPointShot=2;
+var threePointShot=3;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// DON'T MODIFY THIS CODE
+updateTeams();
+var scoreOne;
+var scoreTwo;
+var teamOneName;
+var teamTwoName;
+var threePointShot;
+var twoPointShot;
+var freeThrow;
+$("button").click(function(){
+    var team;
+    team = $(this).attr("team");
+    
+    var points;
+    points = $(this).attr("class");
+    
+    updateScore(team, points);
+});
+
+function updateScore(myTeam, myPoints){
+    if(myTeam === "one"){
+        if(myPoints === "twoPt"){
+            scoreOne += twoPointShot;
+        } else if(myPoints === "threePt"){
+            scoreOne += threePointShot;
+        } else if(myPoints === "freeThrow"){
+            scoreOne += freeThrow;
+        }
+        $(".scoreOne").text(scoreOne);
+    } else if (myTeam === "two"){
+        if(myPoints === "twoPt"){
+            scoreTwo += twoPointShot;
+        } else if(myPoints === "threePt"){
+            scoreTwo += threePointShot;
+        } else if(myPoints === "freeThrow"){
+            scoreTwo += freeThrow;
+        }
+        $(".scoreTwo").text(scoreTwo);
+    }
+}
+
+function updateTeams(){
+    $(".teamOne").text(teamOneName);
+    $(".teamTwo").text(teamTwoName);
+}
+
